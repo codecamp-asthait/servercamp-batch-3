602649d97214ba58d103c48a0c805b9c068291d0
Shafayet Maruf <53457684+sftmaruf@users.noreply.github.com>
2026-06-13 06:18:21 +0600

    Class-20: updated migration

---
+++ class-20/Dukaan/Dukaan.Infrastructure/Migrations/20260613001807_addedPhoneColToCustomer.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dukaan.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedPhoneColToCustomer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Customers",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Customers",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
