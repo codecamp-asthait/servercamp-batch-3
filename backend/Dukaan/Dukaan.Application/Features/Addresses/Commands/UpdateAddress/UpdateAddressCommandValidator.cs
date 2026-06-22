dc9c28aeb99c1c14ef43cadb401ee1d7b46b5f18
Shafayet Maruf <53457684+sftmaruf@users.noreply.github.com>
2026-06-22 12:47:25 +0600

    refactor: merge validators into single file

--- backend/Dukaan/Dukaan.Application/Features/Addresses/Commands/UpdateAddress/UpdateAddressCommandValidator.cs
+++ backend/Dukaan/Dukaan.Application/Features/Addresses/Commands/UpdateAddress/UpdateAddressCommandValidator.cs
@@ -1,6 +1,21 @@
+using Dukaan.Application.Features.Addresses.Dtos;
 using FluentValidation;
 
 namespace Dukaan.Application.Features.Addresses.Commands.UpdateAddress;
+
+public class UpdateAddressDataValidator : AbstractValidator<UpdateAddressData>
+{
+    public UpdateAddressDataValidator()
+    {
+        RuleFor(x => x.Label).NotEmpty().WithMessage("Label is required.");
+        RuleFor(x => x.Street).NotEmpty().WithMessage("Street is required.");
+        RuleFor(x => x.City).NotEmpty().WithMessage("City is required.");
+        RuleFor(x => x.District).NotEmpty().WithMessage("District is required.");
+        RuleFor(x => x.PostalCode).NotEmpty().WithMessage("Postal code is required.");
+        RuleFor(x => x.Phone).NotEmpty().WithMessage("Phone is required.");
+        RuleFor(x => x.Phone).Matches(@"^[\d\+\-\s()]+$").WithMessage("Phone must be a valid format.");
+    }
+}
 
 public class UpdateAddressCommandValidator : AbstractValidator<UpdateAddressCommand>
 {
